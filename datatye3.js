8155cdd9bf9b168b4091a711b2c4db55c0c29ec3
OmPrakash-source <omjhade23@gmail.com>
2025-04-19 23:23:04 +0530

    Initial commit

---
+++ datatye3.js
@@ -0,0 +1,10 @@
+const id = Symbol('123')
+const otherId = Symbol('123')
+    // display are convert same value to diffrent value
+console.log(id==otherId)
+
+// refrence or non primitive
+
+    let heros = ["xy","yz","zx"]  // array
+
+
