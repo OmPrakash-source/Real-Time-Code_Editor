9bb8fbc1caa2d43bf55158d236f634298e6d9cf5
OmPrakash-source <your.omjhade23@gmail.com>
2025-12-05 14:56:40 +0530

    fix mongo db

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.get('*', (req, res) => {
 initSocket(io);
 
 // ---- MongoDB Connection ----
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
