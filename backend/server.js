f2e20bca4749d4ccd870779f5b1e536f202fb97c
OmPrakash-source <your.omjhade23@gmail.com>
2026-04-18 00:22:19 +0530

    fix: remove deprecated mongoose options

--- backend/server.js
+++ backend/server.js
@@ -53,10 +53,7 @@ app.get('*', (req, res) => {
 initSocket(io);
 
 // ---- MongoDB Connection ----
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
